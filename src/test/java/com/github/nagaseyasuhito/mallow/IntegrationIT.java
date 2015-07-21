5b2ac238e67aac3860f6c0af97c28fd1fd0ae46a
taiichiro-nagase <taiichiro.nagase@gree.net>
2015-07-21 13:07:54 +0900

    add tests

---
+++ src/test/java/com/github/nagaseyasuhito/mallow/IntegrationIT.java
@@ -0,0 +1,11 @@
+package com.github.nagaseyasuhito.mallow;
+
+import org.junit.Test;
+
+public class IntegrationIT {
+
+	@Test
+	public void test() {
+		System.out.println("This is integration test.");
+	}
+}
---
+++ src/test/java/com/github/nagaseyasuhito/mallow/UnitTest.java
@@ -0,0 +1,11 @@
+package com.github.nagaseyasuhito.mallow;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void test() {
+		System.out.println("This is unit test.");
+	}
+}
