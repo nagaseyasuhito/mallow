7eac9156f94b18d4a2a5675d140fa451db9886ad
taiichiro-nagase <taiichiro.nagase@gree.net>
2015-07-30 18:25:03 +0900

    modify .travis.yml

--- src/test/java/com/github/nagaseyasuhito/mallow/dao/UserDaoTest.java
+++ src/test/java/com/github/nagaseyasuhito/mallow/dao/UserDaoTest.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.github.nagaseyasuhito.mallow.entity.User;
@@ -13,8 +15,10 @@ import com.google.common.collect.Maps;
 
 public class UserDaoTest {
 
-	@Test
-	public void persistSuccess() {
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void before() {
 		// overriding original persistence.xml
 		Map<String, String> properties = Maps.newHashMap();
 		properties.put("javax.persistence.transactionType", "RESOURCE_LOCAL");
@@ -28,18 +32,24 @@ public class UserDaoTest {
 		properties.put("eclipselink.logging.level" + ".sql", "FINE");
 		properties.put("eclipselink.ddl-generation", "create-tables");
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mallow", properties);
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		this.entityManagerFactory = Persistence.createEntityManagerFactory("mallow", properties);
+	}
 
+	@After
+	public void after() {
+		this.entityManagerFactory.close();
+	}
+
+	@Test
+	public void persistSuccess() {
+		EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
 		User user = new User();
-		user.setMailAddress("gymnaster@example.com");
-
+		user.setMailAddress("mallow@example.com");
 		entityManager.persist(user);
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
-		entityManagerFactory.close();
 	}
 }
